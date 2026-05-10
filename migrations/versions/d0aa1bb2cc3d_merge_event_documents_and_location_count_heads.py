d9d194057991aa01a8cfea249303ee8817e3704b
bryce <brycecotton@mail.com>
2026-05-10 16:07:38 -0500

    Merge Alembic heads for event documents and location counts

---
+++ migrations/versions/d0aa1bb2cc3d_merge_event_documents_and_location_count_heads.py
@@ -0,0 +1,21 @@
+"""merge event documents and location count heads
+
+Revision ID: d0aa1bb2cc3d
+Revises: c8e9f0a1b2c3, fb5c6d7e8f9
+Create Date: 2026-05-10 00:00:00.000000
+"""
+
+
+# revision identifiers, used by Alembic.
+revision = "d0aa1bb2cc3d"
+down_revision = ("c8e9f0a1b2c3", "fb5c6d7e8f9")
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
